7ee8421b74d9e1cee1ade11dae3796b92759a5af
Hector Rivas Gandara <keymon@gmail.com>
2015-09-04 16:51:09 +0100

    Allow ruby string interpolation in yaml

--- erb/myspiff.rb
+++ erb/myspiff.rb
@@ -17,6 +17,25 @@ def search_yaml_values(key)
     recursive_search(FINAL_YAML, key.split('.'))
 end
 
+# Executes the string interpolation/expansion in a structure hash
+def perform_yaml_interpolation(structure, interpolation_binding)
+    case structure
+    when String
+        if structure =~ /^\#\{(.*)\}$/
+            # If there is only this interpolation, expand it and insert the value
+            eval $1
+        else
+            # If not, expand as a string
+            eval "\"#{structure}\"" # Don't ask, it works.... %-)
+        end
+    when Hash
+        structure.each { |k, v|
+            structure[k] = perform_yaml_interpolation(v, interpolation_binding)
+        }
+    else
+        structure
+    end
+end
 context_binding = binding()
 
 files.each do |f|
@@ -31,5 +50,7 @@ files.each do |f|
         raise "Unknown file type #{f}"
     end
 end
+
+perform_yaml_interpolation(FINAL_YAML, context_binding)
 
 puts FINAL_YAML.to_yaml
