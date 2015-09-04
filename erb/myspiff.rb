b687cf467f0b6df29c25692ecd4618149aa664d1
Hector Rivas Gandara <keymon@gmail.com>
2015-09-04 11:37:46 +0100

    Allow refer YAML variables from erb templates
    Use dot_hash to be able to read variables from the already processed yaml files.

--- erb/myspiff.rb
+++ erb/myspiff.rb
@@ -2,30 +2,28 @@
 require 'yaml'
 require 'deep_merge' # gem install deep_merge
 require "erb" # gem install erb
-
-yamls = []
+require "dot_hash"
 
 files=ARGV
+
+FINAL_YAML={}
+def yaml_values()
+    FINAL_YAML.to_properties # use dot_hash
+end
 
 context_binding = binding()
 
 files.each do |f|
     if f.downcase =~ /.*\.yml$/
-        yamls << YAML.load(File.read(f))
+        FINAL_YAML.deep_merge!(YAML.load(File.read(f)))
     elsif f.downcase =~ /.*\.yml\.erb$/
         template = ERB.new(File.read(f))
-        yamls << YAML.load(template.result(context_binding))
+        FINAL_YAML.deep_merge!(YAML.load(template.result(context_binding)))
     elsif f.downcase =~ /.*.rb$/
         eval(File.read(f), context_binding)
     else
         raise "Unknown file type #{f}"
-    end     
+    end
 end
 
-final_yaml={}
-
-yamls.each { |y| 
-    final_yaml.deep_merge!(y)
-} 
-
-puts final_yaml.to_yaml
+puts FINAL_YAML.to_yaml
